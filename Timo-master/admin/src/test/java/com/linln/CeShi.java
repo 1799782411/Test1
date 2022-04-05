c1c0da679b34756f7ec50831697db9b1907bdef3
zhanghuiqiang <1799782411@qq.com>
2022-04-05 21:31:36 +0800

    first commit

---
+++ Timo-master/admin/src/test/java/com/linln/CeShi.java
@@ -0,0 +1,59 @@
+package com.linln;
+
+import java.io.FileNotFoundException;
+
+public class CeShi {
+    public static void main(String[] args) {
+        System.out.println("你好呀");
+        /*int size = 27;
+        String name = "Fido";
+        Dog myDog = new Dog(name,size);
+        x= size-5;
+        if(x<15) myDog.bark(8);
+        while (x>3) {
+            myDog.play();
+        }
+        int[] numList = {2,4,6,8};
+
+        System.out.print("Hello");
+        System.out.println("Dog:"+ name);
+        String num = "8";
+        int z = Integer.parseInt(num);
+        int q = Integer.parseInt(num);
+        int s = Integer.parseInt(num);
+        int[] a = {1,2,3,4};
+        int[] b = {3,4,5,6};
+        int[] r = {5,3,2,5};
+        int[] f
+
+
+        try{
+            readTheFile("myFile".txt);
+
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+        }
+        public class MyFirstApp{
+            public static void main(String[] args) {
+                System.out.println("I Rule");
+            }
+        }
+        public class MyFirstApp{
+            public static void main(String[] arg) {
+                System.out.println();
+            }
+        }*/
+        for (int x = 0; x < 10; x = x+1) {
+            System.out.println("x is now" +x);
+        }
+        int x = 10;
+        while(x>0){
+            --x;
+            System.out.println("X值为："+x);
+        }
+        System.out.println("最后的x:"+"x");
+
+        
+    }
+}
